45702c927b43851246653e636083f1b28cda3d09
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2023-12-16 13:21:14 +0200

    ImProve Readingc

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -74,88 +74,90 @@ const Landing = () => {
             </div>
 
             <div>
-                <div class="flex flex-col justify-start mt-0">
+                <div className="flex flex-col justify-start mt-0">
                     <h1 className='flex-1 text-center text-[#20463C]'>Tickets Found</h1>
                     {
                         tickets && tickets.length > 0 ? (
                             <table>
-                                {tickets.map(ticket => (
-                                    <tr className='' key={ticket.id}>
-                                        <td>
-                                            <div>
+                                <tbody>
+                                    {tickets.map(ticket => (
+                                        <tr className='' key={ticket.id}>
+                                            <td>
                                                 <div>
-                                                    <div className="flex h-full flex-col">
-                                                        <div className='px-3 bg-[#F8F8F8] flex justify-between items-center border-[0] border-t border-solid border-[#e2e2e2] mb-3'>
-                                                            <div class="flex items-center">
-                                                                <div
-                                                                    class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-royalblue text-white flex-shrink-0">
-                                                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                                                        stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                                                                        <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-                                                                    </svg>
+                                                    <div>
+                                                        <div className="flex h-full flex-col">
+                                                            <div className='px-3 bg-[#F8F8F8] flex justify-between items-center border-[0] border-t border-solid border-[#e2e2e2] mb-3'>
+                                                                <div className="flex items-center">
+                                                                    <div
+                                                                        className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-royalblue text-white flex-shrink-0">
+                                                                        <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+                                                                            strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                                            <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                                                                        </svg>
+                                                                    </div>
+                                                                    <h3 className="text-base font-light flex space-x-2">
+                                                                        <span>
+                                                                            {ticket.origin.name}, {ticket.origin.abbrev}
+                                                                        </span>
+                                                                        <span>-</span>
+                                                                        <span>
+                                                                            {ticket.destination.name}, {ticket.destination.abbrev}
+                                                                        </span>
+                                                                    </h3>
                                                                 </div>
-                                                                <h3 class="text-base font-light flex space-x-2">
-                                                                    <span>
-                                                                        {ticket.origin.name}, {ticket.origin.abbrev}
-                                                                    </span>
-                                                                    <span>-</span>
-                                                                    <span>
-                                                                        {ticket.destination.name}, {ticket.destination.abbrev}
-                                                                    </span>
-                                                                </h3>
+
+                                                                <div className='flex space-x-2 items-center'>
+                                                                    {ticket.discount &&
+                                                                        <span className='text-xs bg-green-500 bg-opacity-70 hover:bg-opacity-100 text-white rounded-full px-3 py-2'>
+                                                                            {ticket.discount}% OFF
+                                                                        </span>
+                                                                    }
+
+                                                                    <h1 className='text-2xl text-royalblue'>
+                                                                        RWF {ticket.price.toLocaleString()}
+                                                                    </h1>
+                                                                </div>
+
                                                             </div>
 
-                                                            <div className='flex space-x-2 items-center'>
-                                                                {ticket.discount &&
-                                                                    <span className='text-xs bg-green-500 bg-opacity-70 hover:bg-opacity-100 text-white rounded-full px-3 py-2'>
-                                                                        {ticket.discount}% OFF
-                                                                    </span>
-                                                                }
-
-                                                                <h1 className='text-2xl text-royalblue'>
-                                                                    RWF {ticket.price.toLocaleString()}
-                                                                </h1>
-                                                            </div>
-
-                                                        </div>
-
-                                                        <div class="px-3 flex flex-col justify-between flex-grow">
-                                                            <p className="text-xl leading-relaxed m-0">
-                                                                Remaining Tickets: {ticket.remainingPlaces} out of {ticket.defaultPlaces}
-                                                            </p>
-                                                            <div className='flex justify-between'>
-                                                                <p className="flex space-x-5 leading-relaxed text-base">
-                                                                    <span>
-                                                                        Departure - {format(new Date(ticket.departureDateTime), 'HH:mm a')} {ticket.origin.slang}
-                                                                    </span>
-
-                                                                    <span>
-                                                                        Arrival - {format(new Date(ticket.arrivalDateTime), 'HH:mm a')} {ticket.destination.slang}
-                                                                    </span>
+                                                            <div className="px-3 flex flex-col justify-between flex-grow">
+                                                                <p className="text-xl leading-relaxed m-0">
+                                                                    Remaining Tickets: {ticket.remainingPlaces} out of {ticket.defaultPlaces}
                                                                 </p>
-                                                                {ticket.remainingPlaces > 0 ?
-                                                                    (
-                                                                        <Link to="/book/menu2" className="flex self-baseline bg-royalblue rounded-lg  text-white hover:bg-opacity-80 items-center no-underline p-3" onClick={() => { handleSubmit(ticket) }} >
-                                                                            Buy Ticket
-                                                                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                                                                stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                                                                                <path d="M5 12h14M12 5l7 7-7 7"></path>
-                                                                            </svg>
-                                                                        </Link>
-                                                                    ) :
-                                                                    (<span href="#" className="self-baseline bg-red-600 rounded-lg bg-opacity-50 text-white  p-3">
-                                                                        Sold Out
-                                                                    </span>)
-                                                                }
+                                                                <div className='flex justify-between'>
+                                                                    <p className="flex space-x-5 leading-relaxed text-base">
+                                                                        <span>
+                                                                            Departure - {format(new Date(ticket.departureDateTime), 'HH:mm a')} {ticket.origin.slang}
+                                                                        </span>
+
+                                                                        <span>
+                                                                            Arrival - {format(new Date(ticket.arrivalDateTime), 'HH:mm a')} {ticket.destination.slang}
+                                                                        </span>
+                                                                    </p>
+                                                                    {ticket.remainingPlaces > 0 ?
+                                                                        (
+                                                                            <Link to="/book/menu2" className="flex self-baseline bg-royalblue rounded-lg  text-white hover:bg-opacity-80 items-center no-underline p-3" onClick={() => { handleSubmit(ticket) }} >
+                                                                                Buy Ticket
+                                                                                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+                                                                                    strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                                                                                    <path d="M5 12h14M12 5l7 7-7 7"></path>
+                                                                                </svg>
+                                                                            </Link>
+                                                                        ) :
+                                                                        (<span href="#" className="self-baseline bg-red-600 rounded-lg bg-opacity-50 text-white  p-3">
+                                                                            Sold Out
+                                                                        </span>)
+                                                                    }
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </div>
-                                                </div>
 
-                                            </div>
-                                        </td>
-                                    </tr>
-                                ))}
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
                             </table>
                         ) : (<h3 className='flex-1 text-center text-red-600'>No Tickets Matching your search.</h3>)
                     }
--- src/components/layout/TicketPage/TicketPage.js
+++ src/components/layout/TicketPage/TicketPage.js
@@ -37,6 +37,7 @@ export default function TicketPage({ history }) {
         let originalContents = document.body.innerHTML;
         document.body.innerHTML = printContents;
         window.print();
+
         document.body.innerHTML = originalContents;
     }
 
